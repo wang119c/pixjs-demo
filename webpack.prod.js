528eaf1772c7aaed1f54807eb01e7771ffa1f454
huizi <1052430943@qq.com>
2022-05-06 11:24:29 +0800

    修改配置

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,12 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 })
